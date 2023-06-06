c47f3a904c6c0fc0571fec74b5dddc62eee5391e
BradySBaker <bradysamuelbaker@gmail.com>
2023-06-06 15:46:19 -0700

    Raindrop color options and amount option added

---
+++ client/src/rainController.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+import { HexColorPicker } from "react-colorful";
+
+const RainController = ({handleRain, sendData}) => {
+	const [colorChoices, setColorChoices] = useState([]);
+	const [curChosenColor, setCurChosenColor] = useState(color);
+	const handleRainColor = (newColor) => {
+		setCurChosenColor(newColor);
+	};
+
+	const handleChooseColor = () => {
+		var colors = JSON.parse(JSON.stringify(colorChoices));
+		colors.push(curChosenColor);
+		setColorChoices(colors);
+		sendData("CRR" + curChosenColor.slice(1));
+	};
+
+	return (
+		<div id='rainController'>
+			<div className='picker-container'>
+				<HexColorPicker style={{height: 'calc(90vw * 0.5)'}} color={color} onChange={handleRainColor} />
+			</div>
+			<div>
+				{colorChoices.map((curChoice) => {
+					return(
+						<div style={{'backgroundColor': curChoice, 'width': '100px', 'height': '100px'}}></div>
+					)
+				})}
+			</div>
+			<button style={{'color': curChosenColor}} onClick={handleChooseColor}>Choose Color</button>
+			<input id='rainAmount' type='text' placeholder='1...'/>
+			<button onClick={handleRain}>rain</button>
+		</div>
+	)
+};
+
+export default RainController;
